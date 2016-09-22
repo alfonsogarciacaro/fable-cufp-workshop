873b7e8505b327bb0eccacee8cf271de2bd5ac5b
Alfonso Garcia-Caro <alfonso.garcia-caro@hotmail.com>
2016-09-22 22:55:40 +0900

    Add todomvc-fable-arch sample

---
+++ todomvc-fable-arch/webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  devtool: "source-map",
+  entry: "./temp/todomvc.js",
+  output: {
+    path: __dirname,
+    filename: "bundle.js"
+  },
+  module: {
+    preLoaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: "source-map-loader"
+    }],
+    loaders: [{
+      loader: "style-loader!css-loader",
+      test: /\.css$/
+    }]
+  }
+};
